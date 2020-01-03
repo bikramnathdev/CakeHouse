820c170bf10a2169fb33ecdca4f6a83fb2ab26b5
Bikram Nath <bikramnath.office@gmail.com>
2020-01-03 15:53:01 +0530

    new commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,9 +2,10 @@ import React, {Component, Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import Login from './components/admin/auth/Login';
+import Register from './components/admin/auth/Register';
 import Main from './layouts/Main';
+import Shop from './components/shop/Shop';
 
 
 
@@ -15,10 +16,11 @@ export default class App extends Component  {
         <div>
           <Router>
           <Fragment>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <Route exact path="/" component={Main}/>
-            </Fragment>
+                <Route exact path="/admin/login" component={Login}/>
+                <Route exact path="/admin/register" component={Register}/>
+                <Route exact path="/admin" component={Main}/>
+                <Route exact path="/" component={Shop}/>
+          </Fragment>
           </Router>
         </div>
     )
